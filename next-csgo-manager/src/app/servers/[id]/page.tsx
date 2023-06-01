3676a739840b91382d27af42ab392ba67b700107
Gabbeh <31376076+hvgab@users.noreply.github.com>
2023-06-01 21:17:26 +0200

    valve server query api and ServerCard component.

--- next-csgo-manager/src/app/servers/[id]/page.tsx
+++ next-csgo-manager/src/app/servers/[id]/page.tsx
@@ -1,8 +1,4 @@
-const {
-  Server,
-  RCON,
-  MasterServer,
-} = require("@fabricio-191/valve-server-query");
+const { Server, RCON, MasterServer } = require("@fabricio-191/valve-server-query");
 
 const server = {
   id: 1,
@@ -12,21 +8,13 @@ const server = {
   ip: "",
   port: 27015,
   rcon_password: "",
-  imageSrc:
-    "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-  imageAlt:
-    "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+  imageSrc: "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+  imageAlt: "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
 };
 
-const object_thing = [];
-
-Object.entries(server).map(([key, value]) => {
-  // Pretty straightforward - use key for the key and value for the value.
-  // Just to clarify: unlike object destructuring, the parameter names don't matter here.
-  object_thing.push([key, value]);
-});
-
 export default async function ServerDetail({ params }) {
+  console.log("ServerDetail params");
+  console.log(params);
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
--- next-csgo-manager/src/app/servers/page.tsx
+++ next-csgo-manager/src/app/servers/page.tsx
@@ -1,25 +1,34 @@
+import ServerCard from "./server_card";
+
+const { Server, RCON, MasterServer } = require("@fabricio-191/valve-server-query");
+
 const servers = [
   {
     id: 1,
-    name: "Server 1",
     href: "/servers/1",
-    ip: ":27015",
+    host: "theck1.no",
+    port: 27015,
     rcon_password: "",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-    imageAlt:
-      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+    imageSrc: "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
+    imageAlt: "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
   },
   {
     id: 2,
-    name: "Server 2",
     href: "/servers/2",
-    ip: ":27030",
+    host: "theck1.no",
+    port: 27030,
     rcon_password: "",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
-    imageAlt:
-      "Olive drab green insulated bottle with flared screw lid and flat top.",
+    imageSrc: "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
+    imageAlt: "Olive drab green insulated bottle with flared screw lid and flat top.",
+  },
+  {
+    id: 3,
+    href: "/servers/3",
+    host: "csgo.gabbeh.no",
+    port: 27016,
+    rcon_password: "",
+    imageSrc: "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
+    imageAlt: "Olive drab green insulated bottle with flared screw lid and flat top.",
   },
 ];
 
@@ -43,30 +52,10 @@ export default function ServerList() {
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Servers</h2>
 
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {servers.map((server) => (
-            // Initiate both requests in parallel
-            // const serverData = await getServerData(server.id);
-
-            <a key={server.id} href={server.href} className="group">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                <img
-                  src={server.imageSrc}
-                  alt={server.imageAlt}
-                  className="h-full w-full object-cover object-center group-hover:opacity-75"
-                />
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">{server.name}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">
-                {server.ip}
-              </p>
-              <p className="mt-1 text-lg font-medium text-gray-900">
-                {server.rcon_password}
-              </p>
-
-              <ul>{/* <li>{serverData}</li> */}</ul>
-            </a>
-          ))}
+        <div className="grid grid-cols-3">
+          <ServerCard server={servers[0]} />
+          <ServerCard server={servers[1]} />
+          <ServerCard server={servers[2]} />
         </div>
       </div>
     </div>
