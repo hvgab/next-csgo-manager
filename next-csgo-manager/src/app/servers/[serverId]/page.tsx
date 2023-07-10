2de23fb0854dcf31c29b84f2ae43731225d51639
Gabbeh <31376076+hvgab@users.noreply.github.com>
2023-07-10 12:50:50 +0200

    Page Loop Bug

--- next-csgo-manager/src/app/servers/[serverId]/page.tsx
+++ next-csgo-manager/src/app/servers/[serverId]/page.tsx
@@ -6,13 +6,7 @@ import { mapNameToWorkshopId } from "@/app/servers/utils";
 import ServerDbCard from "../ServerDbCard";
 import ServerQueryCard from "../ServerQueryCard";
 
-export default async function ServerDetail({ params: { serverId } }: { params: { serverId: number } }) {
-  // const server = await prisma.server.findUnique({
-  //   where: {
-  //     id: Number(serverId),
-  //   },
-  // });
-
+export default function ServerDetail({ params: { serverId } }: { params: { serverId: number } }) {
   return (
     <>
       <section className="container mx-auto px-40">
--- next-csgo-manager/src/app/servers/[serverId]/rcon/page.tsx
+++ next-csgo-manager/src/app/servers/[serverId]/rcon/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import RconComponent from "../../RconComponent";
-import { prisma } from "../../../lib/database";
-import { mapNameToWorkshopId } from "@/app/servers/utils";
 
-export default async function ServerDetailRcon({ params: { serverId } }: { params: { serverId: number } }) {
+export default function ServerDetailRcon({ params: { serverId } }: { params: { serverId: number } }) {
   console.log(`ServerDetailRcon params: ${serverId}`);
 
   return (
